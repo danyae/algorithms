aa81b82390f8e6efdab4953a7e9f423a30f16d1f
danyae <danyae@users.noreply.github.com>
2020-11-12 22:45:05 +0300

    comments

--- 04-merge-sort-2.js
+++ 04-merge-sort-2.js
@@ -1,40 +1,45 @@
 // сортировка нормального человека
 
 function merge(arr, first, middle, last) {
+    // Представим себе 2 колоды карт
     const L = []
     const R = []
 
+    // Первую половину карт накидаем в левую колоду,
+    // а вторую - в правую
     for (let i = first; i <= middle; i++) {
         L.push(arr[i])
     }
     for (let i = middle + 1; i <= last; i++) {
         R.push(arr[i])
     }
+    // Сигнальные карты дадут нам возможность выйти за границы массива
+    // И всегда будут больше карты из другой колоды, с которой идет сравнение
     L.push(Number.MAX_SAFE_INTEGER)
     R.push(Number.MAX_SAFE_INTEGER)
 
+    // Верхние карты, в левой и правой колоде
     let nextLeft = 0
     let nextRight = 0
     for (let i = first; i <= last; i++) {
+        // Сравниваем верхнюю карту левой колоды с верхней картой правой
         if (L[nextLeft] <= R[nextRight]) {
+            // Левая меньше, перекладываем ее в общую колоду
             arr[i] = L[nextLeft]
             nextLeft++
         }
         else {
+            // Или правая меньше
             arr[i] = R[nextRight]
             nextRight++
         }
     }
 }
 
-function fillTheRest(dest, destStartIndex, destLastIndex, source, sourceCurrentIndex){
-    for(let i = destStartIndex; i <= destLastIndex; i++){
-        dest[i] = source[sourceCurrentIndex]
-        sourceCurrentIndex++
-    }
-}
-
 function mergeSort(arr, first, last) {
+    // Рекурсивно делим массив пополам, пока не останется 1 элемент
+    // Далее собираем массив обратно функцией merge, устанавливая элементы в правильном порядке
+    // На каждом этапе [first-middle] и (middle-last] будут упорядочены
     if (first < last) {
         const middle = Math.floor((first + last) / 2)
         mergeSort(arr, first, middle)
